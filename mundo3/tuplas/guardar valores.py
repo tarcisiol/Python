dd924e9b93cc250bf7e3b0cf72a4611c6ca9aa2c
tarcisiol <tarcisiol2008@gmail.com>
2026-07-23 01:55:19 -0300

    usando tuplas

--- mundo3/tuplas/guardar valores.py
+++ mundo3/tuplas/guardar valores.py
@@ -1,16 +1,14 @@
-vpares = 0
-v9 = 0
-numeros = (int(input('Digite o primeiro valor: ')) , int(input('Digite o segundo valor: ')) , int(input('Digite o terceiro valor: ')) , int(input('Digite o quarto valor: ')))
+numeros = (int(input('Digite o primeiro valor: ')), int(input('Digite o segundo valor: ')), int(input('Digite o terceiro valor: ')), int(input('Digite o quarto valor: ')))
 print('----------------')
-print(f'voce digitou os numeros: {numeros}')
-for cont in numeros:
-    if cont ==9:
-        v9 +=1
-    elif cont %2==0:
-        vpares +=1
-print(f'o valor 3 apareceu na na {numeros.index(3)+1}° posição')
-print(f'o valor 9 apareceu {v9} vezes')
-print(f'foram digitados {vpares} pares')
-    
+print(f'Você digitou os números: {numeros}')
+print(f'O valor 9 apareceu {numeros.count(9)} vezes')
 
+if 3 in numeros:
+    print(f'O valor 3 apareceu na {numeros.index(3) + 1}ª posição')
+else:
+    print('O valor 3 não foi digitado em nenhuma posição')
 
+print('Os valores pares digitados foram: ', end='')
+for n in numeros:
+    if n % 2 == 0:
+        print(n, end=' ')
